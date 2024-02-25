ccf8b908ee8e767542d60afb980a60a09d2be3f3
Luis Eduardo Spindola <eduspindola@live.com>
2024-02-25 00:05:45 -0300

    Adicionar arquivos de projeto.

---
+++ CashierControl/Areas/Identity/Data/Cashier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CashierControl.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the Cashier class
+public class Cashier : IdentityUser
+{
+}
+
---
+++ CashierControl/Areas/Identity/Data/CashierControlContext.cs
@@ -0,0 +1,22 @@
+﻿using CashierControl.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashierControl.Areas.Identity.Data;
+
+public class CashierControlContext : IdentityDbContext<Cashier>
+{
+    public CashierControlContext(DbContextOptions<CashierControlContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
